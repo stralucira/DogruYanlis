96f38b19775e0c865999e22607be93d397b2791d
Başar Oğuz <baoguz@gmail.com>
2017-02-12 16:43:14 +0200

    Text fields and submit button introduced for the add claims view.

--- DogruYanlis/AddClaimsViewController.swift
+++ DogruYanlis/AddClaimsViewController.swift
@@ -28,31 +28,155 @@ class AddClaimsViewController: FormViewController {
     
     var userName: String = ""
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //navigationController?.navigationBar.alpha = 0.5
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.barTintColor = .white
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        navigationController?.navigationBar.tintColor = _greenColor
+    }
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
-
-        //Looks for single or multiple taps.
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(AddClaimsViewController.dismissKeyboard))
-        view.addGestureRecognizer(tap)
         
-        navigationController?.navigationBar.barTintColor = _greenColor
-        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
-        navigationController?.navigationBar.tintColor = .white
+        CheckRow.defaultCellSetup = { cell, row in cell.tintColor = self._greenColor }
         
         form
+            //Header section
             +++ Section(){ section in
                 let header = HeaderFooterView<AddClaimsLogoView>(.class)
                 section.header = header
-                section.header!.height = {260}
+                section.header!.height = { 130 }
             }
+            
+            //Claim count slider section.
             +++ Section()
-                <<< SliderRowInt() {
+                <<< SliderRowInt("sliderRowTag") {
                     $0.title = "How many claims do you want to make?"
                     $0.minimumValue = 1
                     $0.maximumValue = 5
                     $0.steps = 4
-                    $0.value = 5
+                    $0.value = 1
+                }
+            
+            //Claim input section
+            +++ Section("Claims")
+                <<< TextAreaRow("First Claim") {
+                    $0.placeholder = "First Claim"
+                    $0.textAreaHeight = .dynamic(initialTextViewHeight: 40)
+                }
+                <<< CheckRow() {
+                    $0.title = "True"
+                    $0.value = true
+                }
+                
+                <<< TextAreaRow() {
+                    $0.placeholder = "Second Claim"
+                    $0.textAreaHeight = .dynamic(initialTextViewHeight: 40)
+                    $0.hidden = Condition.function(["sliderRowTag"], { form in
+                        let sliderRowValue = (form.rowBy(tag: "sliderRowTag") as? SliderRowInt)?.value
+                        if (sliderRowValue! < 2) {
+                            return true
+                        } else {
+                            return false
+                        }
+                    })
+                }
+                <<< CheckRow() {
+                    $0.hidden = Condition.function(["sliderRowTag"], { form in
+                        let sliderRowValue = (form.rowBy(tag: "sliderRowTag") as? SliderRowInt)?.value
+                        if (sliderRowValue! < 2) {
+                            return true
+                        } else {
+                            return false
+                        }
+                    })
+                    $0.title = "True"
+                    $0.value = true
+                }
+            
+                <<< TextAreaRow() {
+                    $0.placeholder = "Third Claim"
+                    $0.textAreaHeight = .dynamic(initialTextViewHeight: 40)
+                    $0.hidden = Condition.function(["sliderRowTag"], { form in
+                        let sliderRowValue = (form.rowBy(tag: "sliderRowTag") as? SliderRowInt)?.value
+                        if (sliderRowValue! < 3) {
+                            return true
+                        } else {
+                            return false
+                        }
+                    })
+                }
+                <<< CheckRow() {
+                    $0.hidden = Condition.function(["sliderRowTag"], { form in
+                        let sliderRowValue = (form.rowBy(tag: "sliderRowTag") as? SliderRowInt)?.value
+                        if (sliderRowValue! < 3) {
+                            return true
+                        } else {
+                            return false
+                        }
+                    })
+                    $0.title = "True"
+                    $0.value = true
+                }
+        
+                <<< TextAreaRow() {
+                    $0.placeholder = "Fourth Claim"
+                    $0.textAreaHeight = .dynamic(initialTextViewHeight: 40)
+                    $0.hidden = Condition.function(["sliderRowTag"], { form in
+                        let sliderRowValue = (form.rowBy(tag: "sliderRowTag") as? SliderRowInt)?.value
+                        if (sliderRowValue! < 4) {
+                            return true
+                        } else {
+                            return false
+                        }
+                    })
+                }
+                <<< CheckRow() {
+                    $0.hidden = Condition.function(["sliderRowTag"], { form in
+                        let sliderRowValue = (form.rowBy(tag: "sliderRowTag") as? SliderRowInt)?.value
+                        if (sliderRowValue! < 4) {
+                            return true
+                        } else {
+                            return false
+                        }
+                    })
+                    $0.title = "True"
+                    $0.value = true
+                }
+        
+                <<< TextAreaRow() {
+                    $0.placeholder = "Fifth Claim"
+                    $0.textAreaHeight = .dynamic(initialTextViewHeight: 40)
+                    $0.hidden = Condition.function(["sliderRowTag"], { form in
+                        let sliderRowValue = (form.rowBy(tag: "sliderRowTag") as? SliderRowInt)?.value
+                        if (sliderRowValue! < 5) {
+                            return true
+                        } else {
+                            return false
+                        }
+                    })
+                }
+                <<< CheckRow() {
+                    $0.hidden = Condition.function(["sliderRowTag"], { form in
+                        let sliderRowValue = (form.rowBy(tag: "sliderRowTag") as? SliderRowInt)?.value
+                        if (sliderRowValue! < 5) {
+                            return true
+                        } else {
+                            return false
+                        }
+                    })
+                    $0.title = "True"
+                    $0.value = true
+                }
+            +++ Section()
+                <<< ButtonRow() {
+                    $0.title = "Submit"
                 }
     }
     
@@ -68,11 +192,6 @@ class AddClaimsViewController: FormViewController {
     }
     
     //weak var delegate: DataEnteredDelegate? = nil
-    
-    
-    @IBAction func save(_ sender: UIButton) {
-        
-    }
     
     func textFieldsValid() -> Bool {
         return true
@@ -90,19 +209,19 @@ class AddClaimsLogoView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         let imageView = UIImageView(image: UIImage(named: "addYourClaims"))
-        imageView.frame = CGRect(x: 0, y: 100, width: 500, height: 130)
+        imageView.frame = CGRect(x: 0, y: -20, width: 500, height: 130)
         imageView.autoresizingMask = .flexibleWidth
-        self.frame = CGRect(x: 0, y: 100, width: 500, height: 100)
+        self.frame = CGRect(x: 0, y: -20, width: 500, height: 100)
         imageView.contentMode = .scaleAspectFill
         let label = UILabel(frame: CGRect(x:0, y:50, width:500, height: 20))
         label.text = "Make Your Claims"
         label.font = UIFont(name: "Helvetica-Bold", size: 25)
         label.textColor = UIColor.white
-        label.center = CGPoint(x: 340,y: 65)
+        label.center = CGPoint(x: 330, y: 65)
         imageView.addSubview(label)
         self.addSubview(imageView)
     }
-    
+    //Reperable init
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
